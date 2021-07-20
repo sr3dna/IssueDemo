87eaae2e0324fa301095a9480aa92c798deea948
Anders Chan <anderschan@gmail.com>
2021-07-19 19:44:13 -0700

    Data binding not working if dialog is launched from ActionBarItem

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,23 @@
-import { Component, ViewContainerRef } from "@angular/core";
+import { Component, OnInit, ViewContainerRef } from "@angular/core";
 import { ModalDialogOptions, ModalDialogService } from "nativescript-angular";
-import { Page } from "tns-core-modules";
+import { ActionItem, Page } from "tns-core-modules";
 import { TestDialogComponent } from "./dialog/test-dialog.component";
 
 @Component({
     selector: "Home",
     templateUrl: "./home.component.html"
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
     constructor(
         private page: Page,
         protected viewContainerRef: ViewContainerRef,
         protected modalService: ModalDialogService,
     ) {
+    }
+
+    ngOnInit(): void {
+        this.addActionBarItem();
     }
 
     launchDialog() {
@@ -26,5 +30,22 @@ export class HomeComponent {
             .then((args) => {
                 alert(`Item selected = [${args}]`);
             });
+    }
+
+    addActionBarItem() {
+
+        let newActionItem = new ActionItem();
+        newActionItem.text = "Select Item";
+        newActionItem.on("tap", (args) => {
+
+            this.launchDialog();
+
+            // setTimeout(() => {
+            //     this.launchDialog();
+            // }, 1000)
+
+        })
+        this.page.actionBar.actionItems.addItem(newActionItem);
+
     }
 }
